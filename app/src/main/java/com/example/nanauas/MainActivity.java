3eecbaf711703b4b19e7865df41dd2eec051753a
petik <bkl45bogor@gmail.com>
2021-04-27 14:37:44 +0700

    merubah mainActivity

--- app/src/main/java/com/example/nanauas/MainActivity.java
+++ app/src/main/java/com/example/nanauas/MainActivity.java
@@ -2,76 +2,81 @@ package com.example.nanauas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.VoiceInteractor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity  extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private EditText etName;
-    private Button btnLogin;
-
+    private TextView txtJSON;
+    private Button btnJSON;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogin = findViewById(R.id.btnJSON);
-        etName = findViewById(R.id.name);
+        mQueue = Volley.newRequestQueue(this);
+        txtJSON = findViewById(R.id.txtJSON);
+        btnJSON = findViewById(R.id.btnJSON);
 
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnJSON.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                uraiJson();
             }
         });
     }
-    private void login() {
+
+
+    private void uraiJson() {
         String url = "http://192.168.5.226/rest-api/uasjson/show.php";
 
-        if (etName.getText().toString().isEmpty()){
-            Toast.makeText(this, "Nama Harus Di Isi", Toast.LENGTH_SHORT).show();
-        } else {
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (response.contains("1")){
-                                Toast.makeText(getApplicationContext(), "Berhasil Menampilkan Data Kegemaran", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Tidak Berhasil Menampilkan data", Toast.LENGTH_SHORT).show();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("kegemaran");
+
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                JSONObject mahasantri = jsonArray.getJSONObject(i);
+                                String id = mahasantri.getString("id");
+                                String nama = mahasantri.getString("mahasantri_id");
+                                String hobby = mahasantri.getString("hobby");
+
+                                txtJSON.append(
+                                        id+", "+nama+", "+hobby+"\n\n"
+                                );
                             }
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    error.printStackTrace();
-                    Toast.makeText(MainActivity.this, ""+error, Toast.LENGTH_SHORT).show();
-                }
-            }){
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("mahasantri_id", etName.getText().toString());
-                    return params;
-                }
-            };
-            Volley.newRequestQueue(this).add(stringRequest);
-        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Erorr", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(MainActivity.this, ""+error, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mQueue.add(request);
     }
 }
